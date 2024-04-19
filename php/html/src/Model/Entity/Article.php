53c1d40ed3954443a1922d1f6ad1fc817cccca96
aimerzarashi <git.aimerzarashi.com>
2024-04-19 22:01:22 +0900

    Adding a computed field

--- php/html/src/Model/Entity/Article.php
+++ php/html/src/Model/Entity/Article.php
@@ -1,9 +1,11 @@
 <?php
+
 declare(strict_types=1);
 
 namespace App\Model\Entity;
 
 use Cake\ORM\Entity;
+use Cake\Collection\Collection;
 
 /**
  * Article Entity
@@ -41,5 +43,21 @@ class Article extends Entity
         'modified' => true,
         'user' => true,
         'tags' => true,
+        'tag_string' => true,
     ];
+
+    protected function _getTagString()
+    {
+        if (isset($this->_fields['tag_string'])) {
+            return $this->_fields['tag_string'];
+        }
+        if (empty($this->tags)) {
+            return '';
+        }
+        $tags = new Collection($this->tags);
+        $str = $tags->reduce(function ($string, $tag) {
+            return $string . $tag->title . ', ';
+        }, '');
+        return trim($str, ', ');
+    }
 }
