e2cca399f282382c900826f2376cdd350dd6b6e6
aimerzarashi <git.aimerzarashi.com>
2024-04-20 08:29:04 +0900

    Added Persisting the tag string

--- php/html/src/Model/Table/ArticlesTable.php
+++ php/html/src/Model/Table/ArticlesTable.php
@@ -124,6 +124,41 @@ class ArticlesTable extends Table
             // trim slug to maximum length defined in schema
             $entity->slug = substr($sluggedTitle, 0, 191);
         }
+        if ($entity->tag_string) {
+            $entity->tags = $this->_buildTags($entity->tag_string);
+        }
+    }
+
+    protected function _buildTags($tagString)
+    {
+        // Trim tags
+        $newTags = array_map('trim', explode(',', $tagString));
+        // Remove all empty tags
+        $newTags = array_filter($newTags);
+        // Reduce duplicated tags
+        $newTags = array_unique($newTags);
+
+        $out = [];
+        $tags = $this->Tags->find()
+            ->where(['Tags.title IN' => $newTags])
+            ->all();
+
+        // Remove existing tags from the list of new tags.
+        foreach ($tags->extract('title') as $existing) {
+            $index = array_search($existing, $newTags);
+            if ($index !== false) {
+                unset($newTags[$index]);
+            }
+        }
+        // Add existing tags.
+        foreach ($tags as $tag) {
+            $out[] = $tag;
+        }
+        // Add new tags.
+        foreach ($newTags as $tag) {
+            $out[] = $this->Tags->newEntity(['title' => $tag]);
+        }
+        return $out;
     }
 
     public function findTagged(SelectQuery $query, array $tags = []): SelectQuery
